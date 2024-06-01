90d2b5d82365b2a83ebc19a5f0d372d52b743c44
Ana <ana.damnjanovic@mail.utoronto.ca>
2024-06-01 18:59:51 -0400

    updated ragdoll controller to use input actions

--- Last Stand/Assets/Scripts/RagdollPlayerController.cs
+++ Last Stand/Assets/Scripts/RagdollPlayerController.cs
@@ -1,54 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RagdollPlayerController : MonoBehaviour
 {
-    public float speed;
-    public float strafeSpeed;
-    public float jumpForce;
+    [SerializeField]
+    private float m_speed = 100f;
+    [SerializeField]
+    private float m_jumpForce = 100f;
 
+    [SerializeField]
+    private Rigidbody m_hips;
 
-    public Rigidbody hips;
-    public bool isGrounded;
+    private InputActions m_inputActions;
 
-    void Start()
+    private void Awake()
     {
-        hips = GetComponent<Rigidbody>();
+        m_inputActions = new InputActions();
+        m_inputActions.Enable();
+        m_inputActions.Gameplay.Jump.performed += OnJumpPerformed;
     }
 
     private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (null != m_hips && null != m_inputActions)
         {
-            if (Input.GetKey(KeyCode.LeftShift))
+            Vector2 movement = m_inputActions.Gameplay.Movement.ReadValue<Vector2>();
+            Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
+
+            if (moveDirection.sqrMagnitude > 0f)
             {
-                hips.AddForce(hips.transform.forward * speed * 1.5f);
+                m_hips.AddForce(moveDirection * m_speed);
+                //transform.forward = Vector3.Slerp(transform.forward, moveDirection, 0.15f);
+            }
+        }
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (IsGrounded())
+            {
+                m_hips.AddForce(new Vector3(0, m_jumpForce, 0));
             }
             else
             {
-                hips.AddForce(hips.transform.forward * speed);
+                Debug.Log("not grounded");
             }
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            hips.AddForce(-hips.transform.right * strafeSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            hips.AddForce(-hips.transform.forward * speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            hips.AddForce(hips.transform.right * strafeSpeed);
-        }
-        if(Input.GetAxis("Jump") > 0)
-        {
-            if(isGrounded)
-            {
-                hips.AddForce(new Vector3(0, jumpForce, 0));
-                isGrounded = false;
-            }
-        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.5f);
     }
 }
